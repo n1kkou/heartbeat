dcdddf84589a0065d0131f3c4481d337152f9cb5
Luc <luciaannn@gmail.com>
2018-08-16 13:20:56 +0300

    Create heartbeat.js

---
+++ heartbeat.js
@@ -0,0 +1,28 @@
+// time accumulator in seconds
+var timer = 0; 
+
+// reset timer on mouse move
+document.addEventListener('mousemove', function(){
+	timer = 0;
+});
+
+// reset timer on keypress
+document.addEventListener('keypress', function(){
+	timer = 0;
+});
+
+/**
+* Run the function every second and compare the idle duration with the time(accumulator)
+*/
+function heartbeat(idleDuration){
+	var interval = setInterval(function(){
+  	if(timer >= idleDuration){
+    	alert("Your session expired!");
+      clearInterval(interval);
+    }else{
+    	timer += 1;  
+    }
+  }, 1000);
+}
+
+heartbeat(60);
